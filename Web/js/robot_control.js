ed8b6496c5d69aa6a713d9b62ae6bd4291fcd428
smagnus89 <sergey.tretiyak@gmail.com>
2015-05-02 00:58:32 +0300

    zzz

--- Web/js/robot_control.js
+++ Web/js/robot_control.js
@@ -2,29 +2,40 @@
  * Created by Sergey on 01/05/2015.
  */
 
+function database_connect(url) {
+
+    console.log("Firebase addr = " + url);
+    var ref = new Firebase(url);
+    return ref;
+}
+
+function database_retreive_users(users_ref) {
+
+    // Attach an asynchronous callback to read the data at our posts reference
+    users_ref.on("value", function(snapshot) {
+        for (var key in snapshot.val()) {
+            console.log(key);
+            $("#robot_id_select").append(
+                "<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"#\" id=\"" +
+                key +
+                "\">" + key + "</a></li>");
+        }
+    });
+}
+
 $(document).ready(function(){
 
     // jQuery methods go here...
 
     // Get a reference to our posts
+    var ref;
+    var users_ref;
 
     $("#firebaseConnectButton").click(function (){
 
-        var firebase_addr = $("#server_addr").val();
-        console.log("Firebase addr = " + firebase_addr);
-
-        var ref = new Firebase(firebase_addr);
-        var users_ref = ref.child("users");
-        // Attach an asynchronous callback to read the data at our posts reference
-        users_ref.on("value", function(snapshot) {
-            for (var key in snapshot.val()) {
-                console.log(key);
-                $("#robot_id_select").append(
-                    "<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"#\" id=\"" +
-                    key +
-                    "\">" + key + "</a></li>");
-            }
-        });
+        ref = database_connect($("#server_addr").val());
+        users_ref = ref.child("users");
+        database_retreive_users(users_ref);
 
     });
 
@@ -35,7 +46,10 @@ $(document).ready(function(){
         //console.log("zzzz");
         //console.log($(this).text());
         current_robot = $(this).text();
+        $("#dropdownMenu1").text(current_robot);
         console.log("Robot with id = " + current_robot + " selected.");
+
+        //users_ref.child(current_robot + "robot_respone").on("child_changed")
     }));
 
 
