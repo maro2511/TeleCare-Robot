d709534f292f277c20f9b56b9ce361a8a1bf31aa
smagnus89 <sergey.tretiyak@gmail.com>
2015-05-01 23:22:59 +0300

    connection and list population

--- Web/js/robot_control.js
+++ Web/js/robot_control.js
@@ -7,15 +7,39 @@ $(document).ready(function(){
     // jQuery methods go here...
 
     // Get a reference to our posts
-    var ref = new Firebase("https://torrid-heat-7030.firebaseio.com/users");
-// Attach an asynchronous callback to read the data at our posts reference
-    ref.on("value", function(snapshot) {
-        for (var key in snapshot.val()) {
-            console.log(key);
-            $("#robot_id_select").append(
-                "<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"#\">" + key + "</a></li>");
-        }
+
+    $("#firebaseConnectButton").click(function (){
+
+        var firebase_addr = $("#server_addr").val();
+        console.log("Firebase addr = " + firebase_addr);
+
+        var ref = new Firebase(firebase_addr);
+        var users_ref = ref.child("users");
+        // Attach an asynchronous callback to read the data at our posts reference
+        users_ref.on("value", function(snapshot) {
+            for (var key in snapshot.val()) {
+                console.log(key);
+                $("#robot_id_select").append(
+                    "<li role=\"presentation\"><a role=\"menuitem\" tabindex=\"-1\" href=\"#\" id=\"" +
+                    key +
+                    "\">" + key + "</a></li>");
+            }
+        });
+
     });
+
+
+    var current_robot;
+
+    $("#robot_id_select").on("click", "a", (function (){
+        //console.log("zzzz");
+        //console.log($(this).text());
+        current_robot = $(this).text();
+        console.log("Robot with id = " + current_robot + " selected.");
+    }));
+
+
+    //$("#server_addr").
 
 
 
